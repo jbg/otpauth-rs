25b522805ed36ee37f96a9f08b75e69d1a8ad3bc
Jasper Bryant-Greene <jbg@rf.net.nz>
2019-08-07 12:28:29 +0100

    Revert "Upgrade ring to 0.16.x"
    This reverts commit 8072bb4bb87e5425fce778765407ddf2d6b0d5bc.

--- src/hotp.rs
+++ src/hotp.rs
@@ -16,7 +16,7 @@ use std::io::Cursor;
 
 use byteorder::{BigEndian, ReadBytesExt};
 use base32::Alphabet::RFC4648;
-use ring::hmac;
+use ring::{digest, hmac};
 
 
 /// Two-step verfication of HOTP algorithm.
@@ -45,7 +45,7 @@ impl HOTP {
     ///
     /// ``counter``: HOTP is a counter based algorithm.
     pub fn generate(&self, counter: u64) -> u32 {
-        let key = hmac::Key::new(hmac::HMAC_SHA1_FOR_LEGACY_USE_ONLY, &self.secret);
+        let key = hmac::SigningKey::new(&digest::SHA1, &self.secret);
         let wtr = counter.to_be_bytes().to_vec();
         let result = hmac::sign(&key, &wtr);
         let digest = result.as_ref();
